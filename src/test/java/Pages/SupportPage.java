430e7457984b948cf2ba23e4af5fad172999858b
mindc0re1 <mrkursin@vk.com>
2025-04-02 23:31:11 +0100

    Full_contactus_Test - Page Obejcts sucsessfull tests

--- src/test/java/Pages/SupportPage.java
+++ src/test/java/Pages/SupportPage.java
@@ -1,50 +1,58 @@
 package Pages;
 
 import com.codeborne.selenide.SelenideElement;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
 
 public class SupportPage {
-private final String TITLE_TEXT = "Support";
-private SelenideElement
-        lastNameInput = $("#input_1_5_6"),
-        firstNameInput = $("#input_1_5_3"),
-        phoneInput = $("#input_1_6"),
-        companyInput = $("#input_1_8");
+    private final String TITLE_TEXT = "Support";
+    private SelenideElement
+            lastNameInput = $("#input_1_5_6"),
+            firstNameInput = $("#input_1_5_3"),
+            phoneInput = $("#input_1_6"),
+            companyInput = $("#input_1_8"),
+            emailInput = $("#input_1_7"),
+            descriptionInput = $("#input_1_3");
 
     public void openPage() {
-
-        //Unsucsessfull registration
-    open("https://www.croxyproxy.com/");
-    $(".fc-button-label").click();
-    $(".form-control.input-lg.js-typeahead-domains").setValue("DAXTRA.COM/SUPPORT");
-    $("#requestSubmit").click();
-    //sleep(2000);
+        open("https://www.croxyproxy.com/");
+        $(".fc-button-label").click();
+        $(".form-control.input-lg.js-typeahead-domains").setValue("DAXTRA.COM/SUPPORT");
+        $("#requestSubmit").click();
         $(".wp-block-b1-header__info").shouldHave(text(TITLE_TEXT));
-
-}
+    }
 
     public void setFirstName(String value) {
         firstNameInput.setValue(value);
-
     }
+
     public void setLastName(String value) {
         lastNameInput.setValue(value);
-
     }
 
     public void setPhone(String value) {
         phoneInput.setValue(value);
-
-    }
-        public void setCompany(String value) {
-            companyInput.setValue(value);
-
     }
 
-//Registration is successfull
+    public void setCompany(String value) {
+        companyInput.setValue(value);
+    }
+
+    public void openPageSuccess() {
+        open("https://www.croxyproxy.com/");
+        //$(".fc-button-label").click();
+        $(".form-control.input-lg.js-typeahead-domains").setValue("DAXTRA.COM/SUPPORT");
+        $("#requestSubmit").click();
+        $(".wp-block-b1-header__info").shouldHave(text(TITLE_TEXT));
+    }
+
+    public void setEmail(String value) {
+        emailInput.setValue(value);
+    }
+
+    public void setDescription(String value) {
+        descriptionInput.setValue(value);
+
+
 }
-
-
+}
--- src/test/java/junit/ContactWithPageObjects.java
+++ src/test/java/junit/ContactWithPageObjects.java
@@ -4,69 +4,67 @@ import Pages.SupportPage;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
-
 public class ContactWithPageObjects {
-SupportPage supportPage = new SupportPage();
+    SupportPage supportPage = new SupportPage();
+
     @BeforeAll
     static void beforeAll() {
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "2560x1140";
-
     }
 
     @Test
     void Contact() {
+        // Unsuccessful submission test
         supportPage.openPage();
+        //supportPage.openPageSuccess();
         supportPage.setFirstName("First Name");
         supportPage.setLastName("Last Name");
         supportPage.setPhone("Phone");
         supportPage.setCompany("Company");
 
 
-      //   $("#input_1_5_3").setValue("First Name");
-      //  $("#input_1_5_6").setValue("Last Name");
-      //  $("#input_1_6").setValue("Phone");
-       // $("#input_1_7").setValue("E-mail");
-       // $("#input_1_8").setValue("Company");
         $("#input_1_4").uploadFromClasspath("img/1.jpg");
         $("#input_1_4").uploadFile(new File("src/test/resources/img/1.jpg"));
         $("#gform_submit_button_1").click();
-        $(".gform_validation_errors.validation_error").shouldHave(text("There was a problem with your submission. Please review the fields below."));
+        $(".gform_validation_errors.validation_error")
+                .shouldHave(text("There was a problem with your submission. Please review the fields below."));
 
-        //Successful submussion
-        open("https://www.croxyproxy.com/");
-        $(".form-control.input-lg.js-typeahead-domains").setValue("DAXTRA.COM/SUPPORT");
-        $("#requestSubmit").click();
-        sleep(2000);
-        $(".wp-block-b1-header__info").shouldHave(text("Support"));
-        $("#input_1_5_3").setValue("First Name");
-        $("#input_1_5_6").setValue("Last Name");
-        $("#input_1_6").setValue("Phone");
-        $("#input_1_7").setValue("test@grok.com");
-        $("#input_1_8").setValue("Company");
-        $("#input_1_3").setValue("Description");
-        //Issue Category
+        // Successful submission test
+        supportPage.openPageSuccess();
+        supportPage.setEmail("test@grok.com");
+        supportPage.setDescription("Description");
+        supportPage.setFirstName("First Name");
+        supportPage.setLastName("Last Name");
+        supportPage.setPhone("Phone");
+        supportPage.setCompany("Company");
+       // $("#input_1_7").setValue("test@grok.com");
+       // $("#input_1_3").setValue("Description");
+
+        // Issue Category
         $("#input_1_1").click();
         $("#input_1_1").selectOption("P3 – Minor – Isolated User Issues");
-        //Daxtra Product Affected
+
+        // Daxtra Product Affected
         $("#input_1_2").click();
         $("#input_1_2").selectOption("Daxtra Parser (CVX)");
-        //Country
+
+        // Country
         $("#input_1_10").click();
         $("#input_1_10").selectOption("United Kingdom");
-        //Terms
-       $("#choice_1_12_1").click();
+
+        // Terms
+        $("#choice_1_12_1").click();
 
         sleep(2000);
 
-//Ads
+        // Ads
         $("#hs-eu-decline-button").click();
+
         // Scroll to the button and click
         $("#gform_submit_button_1").shouldBe(visible).shouldBe(enabled).click();
         $("#gform_confirmation_message_1").shouldHave(text("Thank you."));
