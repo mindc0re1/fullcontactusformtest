cb7e769067d27efa3c2f89c0ca4bd65f3976de73
mindc0re1 <mrkursin@vk.com>
2025-06-28 20:14:16 +0100

    Added Post CI 10

--- src/test/java/junit/TestBase.java
+++ src/test/java/junit/TestBase.java
@@ -3,15 +3,22 @@ package junit;
 import Pages.SupportPage;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
-    public class TestBase {
+public class TestBase {
         SupportPage supportPage = new SupportPage();
         TestData data = new TestData();
 
         @BeforeAll
         static void beforeAll() {
-            Configuration.headless = true;
+            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+            Configuration.headless = false;
             Configuration.holdBrowserOpen = true;
             Configuration.browserSize = "1920x1080";
             Configuration.baseUrl = "https://www.croxyproxy.com/";
@@ -19,5 +26,15 @@ import org.junit.jupiter.api.BeforeAll;
             Configuration.screenshots = true;
             Configuration.savePageSource = true;
             Configuration.timeout = 20000;
+Configuration.browser = "chrome";
+Configuration.browserVersion = "128.0";
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+
+            Configuration.browserCapabilities = capabilities;
         }
-}
+        }
+
